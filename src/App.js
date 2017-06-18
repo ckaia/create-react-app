3f648645790da05bfb02a0979e970cff1c644ba5
ckaia <kaia.chrysa@gmail.com>
2017-06-19 01:15:15 +0300

    Support addition of task to list after pressing Enter on input. #285

--- src/App.js
+++ src/App.js
@@ -14,34 +14,43 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    // Get user input
-    task = document.querySelector('input').value;
+  componentDidMount() {
+	  document.getElementById('task').addEventListener('keypress', this.handleClick);
+  }
 
-    // Set state
-    this.setState({taskList: this.state.taskList.concat(task)});
+  handleClick(e) {
+    if (document.activeElement.localName === 'button' || e.keyCode === 13) {
+      e.preventDefault();
 
-    // Reset input
-    document.getElementById("task").reset();
+      // Get user input
+      task = document.querySelector('input').value;
+
+      // Set state
+      this.setState({taskList: this.state.taskList.concat(task)});
+
+      // Reset input
+      document.getElementById('task').reset();
+    }
   }
 
   render () {
-    // TO-DO: When the user types the input and accidently presses "Enter"
-    // the page is refreshed which results to loose the list of tasks entered
-    // till that momment. This behanvior should be prevented.
     return (
       <div>
         Add your task below<br />
-        <form id="task">
-          <Input type="text" defaultText="Type your task in here" />
+        <form id='task'>
+          <Input type='text' defaultText='Type your task in here'/>
         </form><br />
 
         Click to add your task<br />
-        <Button clickData={this.handleClick} text="Add" type="submit" /><br /><br />
+        <Button clickData={this.handleClick} text='Add' type='submit' /><br /><br />
 
 				List of tasks added <List items={this.state.taskList} />
       </div>
     )
+  }
+
+  componentWillUnmount() {
+    document.getElementById('task').removeEventListener('keypress', this.handleClick);
   }
 
 }
