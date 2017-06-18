a90c05694d7b5aa4507cc52b607f0c5bf9184f99
ckaia <kaia.chrysa@gmail.com>
2017-06-18 16:27:50 +0300

    Making React Key unique #259

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const List = ({items}) => {
 	const listItems = items.map(
-		(item) => <li key={item.toString()}>{item}</li>
+		(item, index) => <li key={item + '-' + index}>{item}</li>
 	);
 
 	return (
