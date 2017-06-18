51f33436deecb940468b8ade3c739472419702a3
ckaia <kaia.chrysa@gmail.com>
2017-06-18 12:36:50 +0300

    Correcting variable namimg #259

--- src/App.js
+++ src/App.js
@@ -3,46 +3,43 @@ import Input from './components/Input/Input';
 import Button from './components/Button/Button';
 import List from './components/List/List';
 
-let getUserInput;
+let task;
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {userList: []};
+		this.state = {taskList: []};
 
 		// This binding is necessary to make `this` work in the callback
 		this.handleClick = this.handleClick.bind(this);
 	}
 
-	handleClick(e) {
+	handleClick() {
 		// Get user input
-		getUserInput = document.querySelector('input').value;
+		task = document.querySelector('input').value;
 
 		// Set state
-		this.setState({userList: this.state.userList.concat(getUserInput)});
+		this.setState({taskList: this.state.taskList.concat(task)});
 
 		// Reset input
-		document.getElementById("my-Form").reset();
+		document.getElementById("task").reset();
 	}
 
   render () {
-		// TO-DO: When the input type is password then it should not be allowed
-		// the addition of a task to the list?
-		//
 		// TO-DO: When the user types the input and accidently presses "Enter"
 		// the page is refreshed which results to loose the list of tasks entered
 		// till that momment. This behanvior should be prevented.
     return (
       <div>
         Add your task below<br />
-				<form id="my-Form">
+				<form id="task">
 					<Input type="text" defaultText="Type your task in here" />
 				</form><br />
 
         Click to add your task<br />
 				<Button clickData={this.handleClick} text="Add" type="submit" /><br /><br />
 
-				List of tasks added <List items={this.state.userList} />
+				List of tasks added <List items={this.state.taskList} />
       </div>
     )
   }
