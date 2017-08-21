52f001af1ac6005bdeba13b675603ae2bbd4ea45
chrysa <kaia.chrysa@gmail.com>
2017-08-21 16:58:13 +0300

    Editing a task from the task list. Focus on input field is lost when typing a new character. #387

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // packages
 import React, {Component} from 'react';
+import _ from 'lodash';
 
 // components
 import Button from './components/Button/Button';
@@ -29,17 +30,62 @@ class App extends Component {
     this.addTaskOnButtonClick = this.addTaskOnButtonClick.bind(this);
     this.removeTaskOnButtonClick = this.removeTaskOnButtonClick.bind(this);
     this.addTaskOnEnter = this.addTaskOnEnter.bind(this);
+    this.editTask = this.editTask.bind(this);
+    this.allowEditingTask = this.allowEditingTask.bind(this);
+    this.submitEditingTask = this.submitEditingTask.bind(this);
+
+    // variables
+    this.editingTaskIndex = -1;
   }
 
   /**
-   * To handle click when button for add or removed are clicked
+   * Add task to task list. The task is retrieved from the input text.
+   * The input value is reseted after the state update in order the user to add new task again.
    * @param  {string} task user task
    */
-  addTaskOnButtonClick(task) {
-    //const task = document.getElementById('add-task').value;
+  addTaskOnButtonClick() {
+    const task = document.getElementById('add-task').value;
     this.setState({taskList: this.state.taskList.concat([task])});
 
     document.getElementById('input-task').reset();
+  }
+
+  /**
+   * Add task to task list. The task is retrieved by the event that triggers the addition.
+   * The input value is reseted after the state update in order the user to add new task again.
+   * @param  {object} e keyboard event
+   */
+  addTaskOnEnter(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      this.setState({taskList: this.state.taskList.concat(e.target.value)});
+
+      document.getElementById('input-task').reset();
+    }
+  }
+
+  /**
+   * Allow user to edit the task this a specific index
+   * @param  {number} taskIndex Index that points to the task to be edited
+   */
+  allowEditingTask(taskIndex) {
+    this.editingTaskIndex = taskIndex;
+    this.setState({taskList: this.state.taskList});
+  }
+
+  /**
+   * Edit task
+   * @param  {object} e Event
+   */
+  editTask(e) {
+    const editingTaskList = _.clone(this.state.taskList);
+    editingTaskList[this.editingTaskIndex] = e.target.value;
+
+    this.setState({taskList: editingTaskList});
+    // this.setState(prevState => {
+    //   prevState.taskList[this.editingTaskIndex] = e.target.value;
+    //   return {taskList: prevState.taskList};
+    // });
   }
 
   /**
@@ -54,15 +100,13 @@ class App extends Component {
   }
 
   /**
-   * To handle ENTER pressing on add
-   * @param  {object} e keyboard event
+   * Submit task
+   * @param  {object} e Event
    */
-  addTaskOnEnter(e) {
+  submitEditingTask(e) {
     if (e.key === 'Enter') {
-      e.preventDefault();
-      this.setState({taskList: this.state.taskList.concat(e.target.value)});
-
-      document.getElementById('input-task').reset();
+      this.editingTaskIndex = -1;
+      this.setState({taskList: this.state.taskList});
     }
   }
 
@@ -71,19 +115,25 @@ class App extends Component {
    * @return {object} the displayed to-do interactive list
    */
   render() {
-    const listItems = this.state.taskList.map((item, index) => (
-      <li key={`${item}-${index}`}>
-        <Button
-          onClick={this.removeTaskOnButtonClick}
-          text="Remove"
-          type="submit"
-          width="65px"
-          clickData={item}
-        />
-        <span>{' '}</span>
-        <span>{item}</span>
-      </li>
-    ));
+    let taskElem;
+
+    const listItems = this.state.taskList.map((item, index) => {
+      // Task is presented
+      if (this.editingTaskIndex !== index) {
+        taskElem = <textbox onClick={() => this.allowEditingTask(index)}>{item}</textbox>;
+      } else {
+        // Task is editable
+        taskElem = <input type="text" value={item} onChange={this.editTask} onKeyPress={this.submitEditingTask} />;
+      }
+
+      return (
+        <li key={`${item}-${index}`}>
+          <Button onClick={this.removeTaskOnButtonClick} text="Remove" type="submit" width="65px" clickData={item} />
+          <span>{' '}</span>
+          {taskElem}
+        </li>
+      );
+    });
 
     return (
       <div>
@@ -97,7 +147,6 @@ class App extends Component {
             text="Add"
             type="submit"
             color="royalblue"
-            clickData={document.getElementById('add-task') ? document.getElementById('add-task').value : ''}
           />
         </div>
         <div>
